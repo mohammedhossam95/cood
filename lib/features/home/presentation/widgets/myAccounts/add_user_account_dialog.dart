3c9a9fca77cccf4be2ac11265d26c7aa650dfd26
alilotfy4321 <alilotfy4321@gmail.com>
2025-03-19 16:31:54 +0200

    handel add friend dialog color

--- lib/features/home/presentation/widgets/myAccounts/add_user_account_dialog.dart
+++ lib/features/home/presentation/widgets/myAccounts/add_user_account_dialog.dart
@@ -13,8 +13,12 @@ class AddUserAccountDialog extends StatefulWidget {
   final int id;
   final Color color;
   final String? image;
-  const AddUserAccountDialog(
-      {super.key, required this.id, required this.color,this.image,});
+  const AddUserAccountDialog({
+    super.key,
+    required this.id,
+    required this.color,
+    this.image,
+  });
 
   @override
   State<AddUserAccountDialog> createState() => _AddUserAccountDialogState();
@@ -34,43 +38,53 @@ class _AddUserAccountDialogState extends State<AddUserAccountDialog> {
         children: [
 //------------------------------------1
           Positioned(
-            top: 50.h,
+            top: 45.h,
             left: 0,
             right: 0,
+            bottom: 5.h,
             child: Container(
-              padding: EdgeInsets.all(15.w),
               decoration: BoxDecoration(
-                color: widget.color.withOpacity(0.6),
-                borderRadius: BorderRadius.vertical(
-                  bottom: Radius.circular(15.r),
-                ),
+                color: MyColors.white,
+                borderRadius: BorderRadius.circular(20.0.r),
               ),
-              child: Column(
-                children: [
-                  // Input Field
-                  Gaps.vGap30,
-                  MyTextFormField(
-                    controller: addUserAccountController,
-                    hintText: 'addLink'.tr, // For localization
-                    focusNode: FocusNode(),
-                    keyboardType: TextInputType.text,
+              child: Container(
+                padding: EdgeInsets.all(15.w),
+                decoration: BoxDecoration(
+                  color: widget.color.withOpacity(
+                    0.1,
                   ),
-                  Gaps.vGap20,
-                  // Save Button
-                  MyDefaultButton(
-                    onPressed: () {
-                      //ToDo post method
-                      //----consume cubit
-                      //post(id,addUserAccountController);
-                    },
-                    height: 44.h,
-                    width: 128.w,
-                    btnText: "save", // For localization
-                    color: widget.color,
-                  textColor: widget.color == MyColors.socialYellow?MyColors.black:MyColors.white,          
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(15.r),
                   ),
-                  Gaps.vGap10,
-                ],
+                ),
+                child: Column(
+                  children: [
+                    // Input Field
+                    Gaps.vGap30,
+                    MyTextFormField(
+                      controller: addUserAccountController,
+                      hintText: 'addLink'.tr, // For localization
+                      focusNode: FocusNode(),
+                      keyboardType: TextInputType.text,
+                    ),
+                    Gaps.vGap20,
+                    // Save Button
+                    MyDefaultButton(
+                      onPressed: () {
+                        //ToDo post method
+                        //----consume cubit
+                        //post(id,addUserAccountController);
+                      },
+                      height: 44.h,
+                      width: 128.w,
+                      btnText: "save", // For localization
+                      color: widget.color,
+                      textColor: widget.color == MyColors.socialYellow
+                          ? MyColors.black
+                          : MyColors.white,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -89,7 +103,8 @@ class _AddUserAccountDialogState extends State<AddUserAccountDialog> {
                 child: DiffImage(
                   width: 50.w,
                   height: 50.h,
-                  image:widget.image??'https://cood.testworks.top/storage/social-media-icons/1ojm33Ez9jem0ysLxrLzKBk2KCq2YCi6iWtU02Z4.png',
+                  image: widget.image ??
+                      'https://cood.testworks.top/storage/social-media-icons/1ojm33Ez9jem0ysLxrLzKBk2KCq2YCi6iWtU02Z4.png',
                   //userName: 'image',
                 ),
               ),
